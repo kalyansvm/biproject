758c52040f8016134a0b36924b1221f1584bbf87
wuyiding@gmail.com <wuyiding@gmail.com@4aa0982e-3a0c-11de-a5d8-8da2cac0bf50>
2009-05-13 01:48:55 +0000

    polish

--- trunk/BIServer/script/mysql/install.sql
+++ trunk/BIServer/script/mysql/install.sql
@@ -3,13 +3,20 @@ use mysql;
 select host, user, password from user;
 create database biserver;
 show databases;
+/*
+ * grnat biserverdb user to biserver database (the main database)
+ */
 grant all on biserver.* to 'biserverdb'@'localhost';
 /*
  * quit, login as biserverdb
  */
 source biserver_dump.sql
 /*
- * grant biserverdb user to foodmart
+ * grant biserverdb user to foodmart demo database
  */
 grant all on foodmart.* to 'biserverdb'@'localhost';
 
+/*
+ * grant biserverdb user to sugarcrm demo database
+ */
+grant all on sugarcrm.* to 'biserverdb'@'localhost';
